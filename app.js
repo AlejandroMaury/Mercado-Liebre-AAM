8c7db45bcfba586373b5d1edef1240fcfc753446
Alejandro Arango <alejandroarang@gmail.com>
2023-01-19 09:15:06 -0500

    sin responsive

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ const PORT =process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
+app.use(express.static('views'));
 
 app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
-
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
 
 app.listen(PORT,()=>{
     console.log(`Server run puerto on ${PORT}` );
